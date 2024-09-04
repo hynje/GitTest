633911fe576d199ab5f462e4aeab5646ce5a1adb
hynje <hjyoon573@gmai.com>
2024-09-04 17:44:32 +0900

    Initial Commit

---
+++ GitTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GitTest
+//
+//  Created by 윤현제 on 9/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
