f507047b80a340515caded2a83feb352cd8d8931
hynje <hjyoon573@gmai.com>
2024-09-04 17:53:48 +0900

    commit test

--- GitTest/main.cpp
+++ GitTest/main.cpp
@@ -6,9 +6,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
+    cout << "Github Test!!" << endl;
     return 0;
 }
